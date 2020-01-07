bcd1bef8df4dade631f897d2fc04f608fb91f2c9
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-01-06 23:17:34 -0300

    configurando push put e get de user

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'bbb67b21bd05332838d36d39be7638ad',
+    expiresIn: '7d'
+}
--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,11 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'root',
   database: 'gobarber',
   define: {
-    timestamp: true,
+    timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-};
-
-
+}
